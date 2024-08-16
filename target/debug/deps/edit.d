687b42f088105aa3d992593ad48987661a2deed5
andyrudovv <110022906+andyrudovv@users.noreply.github.com>
2024-08-16 11:33:49 +0500

    .

--- target/debug/deps/edit.d
+++ target/debug/deps/edit.d
@@ -1,8 +1,8 @@
-C:\Users\andy\Documents\GitHub\edit\target\debug\deps\edit.exe: src/main.rs src\core\mod.rs src\core\time\mod.rs src\core\editor\mod.rs
+C:\Users\andy\Documents\GitHub\edit\target\debug\deps\edit.exe: src/main.rs src\core\mod.rs src\core\editor\mod.rs src\core\time\mod.rs
 
-C:\Users\andy\Documents\GitHub\edit\target\debug\deps\edit.d: src/main.rs src\core\mod.rs src\core\time\mod.rs src\core\editor\mod.rs
+C:\Users\andy\Documents\GitHub\edit\target\debug\deps\edit.d: src/main.rs src\core\mod.rs src\core\editor\mod.rs src\core\time\mod.rs
 
 src/main.rs:
 src\core\mod.rs:
-src\core\time\mod.rs:
 src\core\editor\mod.rs:
+src\core\time\mod.rs:
