7dc75bfa520bc86313922f0d00220cf537e7ae89
andyrudovv <andyrudovv@gmail.com>
2024-08-30 01:37:45 -0600

    add

---
+++ src/core/editor/config/mod.rs
@@ -0,0 +1,22 @@
+
+
+pub struct EditorSettings {
+    background_color: (u8, u8, u8),
+}
+
+pub struct StatusBarSettings {
+    backgound_color: (u8, u8, u8),
+    separator_symbol: String,
+}
+
+pub struct CommandBarSettings {
+    background_color: (u8, u8, u8),
+    color: (u8, u8, u8),
+}
+
+pub struct CommandsBindings {
+    quit: String,
+    save: String,
+    edit: String,
+}
+
